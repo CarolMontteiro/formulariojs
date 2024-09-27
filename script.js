51cf80cba98e6e7574faf00f114977407976b1cd
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-09-27 16:17:12 -0300

    Criando funções de validação genérica com parâmetros, uma para campos com Nome e outra para campos com Regex. Esta forma evita a repetição de códigos e permite a reusibilidade

--- script.js
+++ script.js
@@ -3,126 +3,67 @@ const form = document.getElementById('form');
 const campo = document.getElementsByClassName('required');
 const span = document.getElementsByClassName('span-required');
 
-form.addEventListener('submit', (event) => {
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault(); 
 
-    event.preventDefault(); 
+// })
 
-    let arrayErros = [];
-
-    var checkEmptyCamps = false;
-
-    for (let index = 0; index < campo.length; index++) {
-        
-        if(campo[index].value === '') {
-
-            alert('existe campos vazios');
-
-            checkEmptyCamps = true;
-            
-            break;
-        }
-    }
-
-    if(!checkEmptyCamps) {
-
-        for (let index = 0; index < span.length; index++) {
-
-            if(!span[index].textContent.trim() === '') {
-    
-                setErro(index);
-
-                arrayErros.push(span[index].textContent);
-            }
-        }
-    }
-
-    console.log(arrayErros);
-
-    if(arrayErros.length === 0) {
-
-        form.submit();
-    }
-})
-
-function bloquearLetras(event) {
+function blockLetters(event) {
     const key = event.key;
     if (!/[0-9]/.test(key) && key !== 'Backspace' && key !== 'Delete' && key !== 'ArrowLeft' && key !== 'ArrowRight' && key !== 'Tab') {
       event.preventDefault();
     }
 }
 
-function setErro(index) {
+function setError(index, mensage) {
     campo[index].style.border = '2px solid #e63636';
     span[index].style.display = 'block';
+    span[index].textContent = mensage;
 }
 
-function removeErro(index) {
+function removeError(index) {
     campo[index].style.border = '';
     span[index].style.display = 'none';
+    span[index].textContent = '';
 }
 
-function validarCampoNome(idCampo, indiceSpan) {
-    const campoValor = document.getElementById(idCampo).value;
-    
-    if (campoValor.trim() === "") {
-        setErro(indiceSpan);
-        span[indiceSpan].textContent = "O campo não pode estar vazio";
-    } else if (campoValor.length < 3) {
-        setErro(indiceSpan);
-        span[indiceSpan].textContent = "O nome deve ter no mínimo 3 caracteres";
+function nameValidateField(index, valueName) {
+    if (valueName.trim() === "") {
+        setError(index, "O campo não pode estar vazio");
+    } else if (valueName.length < 3) {
+        setError(index, "O nome deve ter no mínimo 3 caracteres");
     } else {
-        removeErro(indiceSpan);
-        span[indiceSpan].textContent = "";
+        removeError(index);
     }
 }
 
-// function nameValidate() {
-//     const nome = document.getElementById('nome').value;
-//     if(nome.trim() === ""){
-//         setErro(0);
-//         span[0].textContent = "O campo não pode estar vazio";
-        
-//     }else if(campo[0].value.length < 3) {
-//         setErro(0);
-//         span[0].textContent = "O nome deve ter no mínimo 3 caracteres";
-//     }else{
-//         removeErro(0);
-//         span[0].textContent = "";
-//     }
-// }
+function validarNome(inputId, index) {
+    const valor = document.getElementById(inputId).value;
+    nameValidateField(index, valor);
+}
+
+function validateField(inputId, index, regex, mensageErro) {
+    const valueField = document.getElementById(inputId).value;
+
+    if (valueField.trim() === "") {
+        setError(index, "O campo não pode estar vazio");
+    } else if (!regex.test(valueField)) {
+        setError(index, mensageErro);
+    } else {
+        removeError(index);
+    }
+}
 
 function emailValidate() {
-    const email = document.getElementById('email').value;
     const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/;
-
-    if(email === "") {
-        setErro(1);
-        span[1].textContent = "O campo não pode estar vazio";   
-    }else if(emailRegex.test(campo[1].value)) {
-        removeErro(1);
-        span[1].textContent = "";   
-    }else {
-        setErro(1);
-        span[1].textContent = "Adicione um email válido";
-    }
+    validateField('email', 1, emailRegex, "Adicione um email válido");
 }
 
 function birthData() {
-    const birth = document.getElementById('date').value;
     const birthRegex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
-    
-    if(birth === "") {
-        setErro(2);
-        span[2].textContent = "O campo não pode estar vazio";
-    }else if(birthRegex.test(campo[2].value)) {
-        removeErro(2);
-        span[2].textContent = "";
-    }else {
-        setErro(2);
-        span[2].textContent = "Adicione uma data valido";
-    }
+    validateField('date', 2, birthRegex, "Adicione uma data válida");
 
-    date.addEventListener('keypress', () => { // O keypress, dispara quando uma tecla que retorna um valor de caractere é pressionada. Por exemplo, se a tecla "a" for pressionada, o evento disparará a letra "a", que retorna o valor 97. Este evento não funciona quando a tecla "shift" é pressionada, pois ela não retorna um valor.
+    date.addEventListener('keypress', () => { 
         let dateLength = date.value.length;
 
         if(dateLength === 2 || dateLength === 5) {
@@ -132,20 +73,9 @@ function birthData() {
 }
 
 function cpfValidate() {
-    const cpfValidate = document.getElementById('cpf').value;
+
     const cpfRegex = /^([0-9]){3}\.([0-9]){3}\.([0-9]){3}-([0-9]){2}$/;
-    
-    if(cpfValidate === "") {
-        setErro(3);
-        span[3].textContent = "O campo não pode estar vazio";
-    }else if(cpfRegex.test(campo[3].value)) {
-        console.log('valido')
-        removeErro(3);
-        span[3].textContent = "";
-    }else {
-        setErro(3);
-        span[3].textContent = "Adicione o cpf valido";
-    }
+    validateField('cpf', 3, cpfRegex, "CPF inválido")
 
     cpf.addEventListener('keypress', () => {
         let cpfLength = cpf.value.length;
@@ -159,64 +89,32 @@ function cpfValidate() {
 }
 
 function telValidate() {
-    const telValidate = document.getElementById('tel').value;
-    const telRegex = /(\(\d{2}\)\s)(\d{4,5}\-\d{4})/;
-    
-    if(telValidate === "") {
-        setErro(4);
-        span[4].textContent = "O campo não pode estar vazio";
-    }else if(campo[4].value.length < 14) {
-        setErro(4);
-        span[4].textContent = "Adicione um número valido";
-    }else if(!telRegex.test(campo[4].value)) {
-        removeErro(4);
-        span[4].textContent = "";
-    }
 
-    tel.addEventListener('input', () => { // O addEventListener, registrar uma espera de evento
+    const telRegex = /(\(\d{2}\)\s)(\d{4,5}\-\d{4})/;
+    validateField('tel', 4, telRegex, "Adicione um número válido")
+
+    tel.addEventListener('input', () => {
         let telLength = tel.value;
 
-        if(telLength.length == 2) {
+        if(telLength.length === 2) {
             tel.value += ') ';
             tel.value = '(' + tel.value;
-        }else if( telLength.length == 10) {
+        }else if( telLength.length === 10) {
             tel.value += '-';
         }
     });
 }
 
-// function nameMotherValidate() {
-//     const nomeMae = document.getElementById('nomemae').value;
-//     if(nomeMae.trim() === ""){
-//         setErro(5);
-//         span[5].textContent = "O campo não pode estar vazio";
-        
-//     }else if(campo[5].value.length < 3) {
-//         setErro(5);
-//         span[5].textContent = "O nome deve ter no mínimo 3 caracteres";
-//     }else{
-//         removeErro(5);
-//         span[5].textContent = "";
-//     }
-// }
-
 //        ENDEREÇO             #######################################################################################
 
 function cepValidate() {
-    const cepValidate = document.getElementById('cep').value;
-    
-    if(cepValidate === "") {
-        setErro(6);
-        span[6].textContent = "O campo não pode estar vazio";
-    }else {
-        removeErro(6);
-        span[6].textContent = "";
-    }
+    const cepRegex = /\d{5}-\d{3}/;
+    validateField('cep', 6, cepRegex, "CEP inválido")
 
-    cep.addEventListener('input', () => { // O addEventListener, registrar uma espera de evento
+    cep.addEventListener('input', () => { 
         let cepLength = cep.value;
 
-        if(cepLength.length == 5) {
+        if(cepLength.length === 5) {
             cep.value += '-';
         }
     });
